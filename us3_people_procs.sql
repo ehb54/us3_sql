a927bb22c822e5619215250f3630aadffc0dbfa7
alexsav815 <alexsav.science@gmail.com>
2023-05-18 21:03:01 -0500

    [SQL for] -- GMP Report e-Signer -- further dev [1].... ...

--- us3_people_procs.sql
+++ us3_people_procs.sql
@@ -172,9 +172,66 @@ BEGIN
                fname AS firstName,
                organization
       FROM     people
-      WHERE    lname LIKE template
-      OR       fname LIKE template
-      AND      gmpReviewer = 1
+      WHERE    (gmpReviewer = 1)
+      AND      (lname LIKE template OR fname LIKE template) 
+      ORDER BY lname, fname;
+    
+    END IF;
+
+  ELSE
+    SELECT @US3_LAST_ERRNO AS status;
+
+  END IF;
+
+END$$
+
+
+-- Lists all ID's and names in the database [ ![NOT] global reviewers] optionally matching
+--   some text in the last name field
+DROP PROCEDURE IF EXISTS get_people_inv$$
+CREATE PROCEDURE get_people_inv( p_personGUID CHAR(36),
+                             	  p_password   VARCHAR(80),
+                             	  p_template   VARCHAR(30) )
+  READS SQL DATA
+
+BEGIN
+  DECLARE template VARCHAR(40);
+
+  CALL config();
+  SET @US3_LAST_ERRNO = @OK;
+  SET @US3_LAST_ERROR = '';
+  SET p_template      = TRIM( p_template );
+
+  IF ( verify_user( p_personGUID, p_password ) = @OK ) THEN
+    IF ( count_people( p_personGUID, p_password, p_template )  < 1 ) THEN
+      SET @US3_LAST_ERRNO = @NOROWS;
+      SET @US3_LAST_ERROR = 'MySQL: no rows returned';
+
+      SELECT @US3_LAST_ERRNO AS status;
+
+    ELSEIF ( LENGTH(p_template) = 0 ) THEN
+      SELECT @OK AS status;
+
+      SELECT   personID,
+               lname AS lastName,
+               fname AS firstName,
+               organization
+      FROM     people
+      WHERE    gmpReviewer = 0
+      ORDER BY lname;
+
+    ELSE
+      SELECT @OK AS status;
+
+      SET template = CONCAT('%', p_template, '%');
+
+      SELECT   personID,
+               lname AS lastName,
+               fname AS firstName,
+               organization
+      FROM     people
+      WHERE    (gmpReviewer = 0)
+      AND      (lname LIKE template OR fname LIKE template) 
       ORDER BY lname, fname;
     
     END IF;
@@ -189,6 +246,10 @@ END$$
 
 
 
+
+
+
+ 
 -- Returns a more complete list of information about one user
 DROP PROCEDURE IF EXISTS get_person_info$$
 CREATE PROCEDURE get_person_info( p_personGUID CHAR(36),
@@ -241,6 +302,96 @@ BEGIN
   END IF;
 
 END$$
+
+
+
+-- set GReviewer based in ID ------------------------------------
+DROP PROCEDURE IF EXISTS set_person_grev_status$$
+CREATE PROCEDURE set_person_grev_status( p_personGUID CHAR(36),
+                                  	 p_password   VARCHAR(80),
+                                  	 p_ID         INT )
+  MODIFIES SQL DATA
+
+BEGIN
+  DECLARE count_person INT;
+
+  CALL config();
+  SET @US3_LAST_ERRNO = @OK;
+  SET @US3_LAST_ERROR = '';
+
+  SELECT     COUNT(*)
+  INTO       count_person
+  FROM       people
+  WHERE      personID = p_ID;
+
+  IF ( verify_user( p_personGUID, p_password ) = @OK ) THEN
+    IF ( count_person = 0 ) THEN
+      SET @US3_LAST_ERRNO = @NOROWS;
+      SET @US3_LAST_ERROR = 'MySQL: no rows returned';
+
+      SELECT @US3_LAST_ERRNO AS status;
+
+    ELSE
+      SELECT @OK AS status;
+
+      UPDATE   people
+      SET      gmpReviewer = 1 
+      WHERE    personID = p_ID;
+
+    END IF;
+
+  ELSE
+    SELECT @US3_LAST_ERRNO AS status;
+
+  END IF;
+
+END$$
+
+
+-- [UN]set GReviewer based in ID ------------------------------------
+DROP PROCEDURE IF EXISTS unset_person_grev_status$$
+CREATE PROCEDURE unset_person_grev_status( p_personGUID CHAR(36),
+                                  	   p_password   VARCHAR(80),
+                                  	   p_ID         INT )
+  MODIFIES SQL DATA
+
+BEGIN
+  DECLARE count_person INT;
+
+  CALL config();
+  SET @US3_LAST_ERRNO = @OK;
+  SET @US3_LAST_ERROR = '';
+
+  SELECT     COUNT(*)
+  INTO       count_person
+  FROM       people
+  WHERE      personID = p_ID;
+
+  IF ( verify_user( p_personGUID, p_password ) = @OK ) THEN
+    IF ( count_person = 0 ) THEN
+      SET @US3_LAST_ERRNO = @NOROWS;
+      SET @US3_LAST_ERROR = 'MySQL: no rows returned';
+
+      SELECT @US3_LAST_ERRNO AS status;
+
+    ELSE
+      SELECT @OK AS status;
+
+      UPDATE   people
+      SET      gmpReviewer = 0 
+      WHERE    personID = p_ID;
+
+    END IF;
+
+  ELSE
+    SELECT @US3_LAST_ERRNO AS status;
+
+  END IF;
+
+END$$
+
+
+
 
 -- INSERTs a new person with the specified information
 DROP PROCEDURE IF EXISTS new_person$$
