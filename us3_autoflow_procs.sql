9a745647f2fcf6f878180c1cd1ff64dab1446f7b
ehb54 <ehb54@users.noreply.github.com>
2023-12-02 04:16:07 +0000

    Update us3_autoflow_procs.sql
    Spurious < removed

--- us3_autoflow_procs.sql
+++ us3_autoflow_procs.sql
@@ -2143,7 +2143,7 @@ BEGIN
     INSERT INTO autoflowReportItem SET
       reportGUID        = p_reportguid,
       reportID          = p_reportid,
-<      type              = p_type,
+      type              = p_type,
       method            = p_method,
       rangeLow          = p_low,	
       rangeHi           = p_hi,
